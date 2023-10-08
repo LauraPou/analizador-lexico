3436221dfd52bc5c0ee397b524d33abe799b8ea0
Jimena Ace <acevedo.romo.jimena.olivia@gmail.com>
2023-10-07 21:50:28 -0600

    Adicion de un tipo de token

--- TipoToken.java
+++ TipoToken.java
@@ -14,7 +14,7 @@ public enum TipoToken {
 
     // Palabras clave
     AND, ELSE, FALSE, FUN, FOR, IF, NULL, OR,
-    PRINT, RETURN, TRUE, VAR, WHILE,
+    PRINT, RETURN, TRUE, VAR, WHILE, COMMENTS,
 
     EOF
 }
