66e76a474124c45475f2f7d8b1052e4d10543df8
Jimena Ace <acevedo.romo.jimena.olivia@gmail.com>
2023-10-07 21:55:10 -0600

    Adicion estado 0

--- Scanner.java
+++ Scanner.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,19 +11,19 @@ public class Scanner {
 
     static {
         palabrasReservadas = new HashMap<>();
-        palabrasReservadas.put("and",    TipoToken.AND);
-        palabrasReservadas.put("else",   TipoToken.ELSE);
-        palabrasReservadas.put("false",  TipoToken.FALSE);
-        palabrasReservadas.put("for",    TipoToken.FOR);
-        palabrasReservadas.put("fun",    TipoToken.FUN);
-        palabrasReservadas.put("if",     TipoToken.IF);
-        palabrasReservadas.put("null",   TipoToken.NULL);
-        palabrasReservadas.put("or",     TipoToken.OR);
-        palabrasReservadas.put("print",  TipoToken.PRINT);
+        palabrasReservadas.put("and", TipoToken.AND);
+        palabrasReservadas.put("else", TipoToken.ELSE);
+        palabrasReservadas.put("false", TipoToken.FALSE);
+        palabrasReservadas.put("for", TipoToken.FOR);
+        palabrasReservadas.put("fun", TipoToken.FUN);
+        palabrasReservadas.put("if", TipoToken.IF);
+        palabrasReservadas.put("null", TipoToken.NULL);
+        palabrasReservadas.put("or", TipoToken.OR);
+        palabrasReservadas.put("print", TipoToken.PRINT);
         palabrasReservadas.put("return", TipoToken.RETURN);
-        palabrasReservadas.put("true",   TipoToken.TRUE);
-        palabrasReservadas.put("var",    TipoToken.VAR);
-        palabrasReservadas.put("while",  TipoToken.WHILE);
+        palabrasReservadas.put("true", TipoToken.TRUE);
+        palabrasReservadas.put("var", TipoToken.VAR);
+        palabrasReservadas.put("while", TipoToken.WHILE);
 
         oprelacionales = new HashMap<>();
         oprelacionales.put("(", TipoToken.LEFT_PAREN);
@@ -42,8 +43,10 @@ public class Scanner {
     private final String source;
 
     private final List<Token> tokens = new ArrayList<>();
-    
-    public Scanner(String source){
+
+    List<String> se = Arrays.asList("(", ")", "{", "}", ",", ".", ";", "+", "-", ";", "[", "]", "*");
+
+    public Scanner(String source) {
         this.source = source + " ";
     }
 
@@ -52,82 +55,60 @@ public class Scanner {
         String lexema = "";
         char c;
 
-        for(int i=0; i<source.length(); i++){
+        for (int i = 0; i < source.length(); i++) {
             c = source.charAt(i);
 
-            switch (estado){
-                case 0:
-                    if(Character.isLetter(c)){
-                        estado = 13;
+            switch (estado) {
+                case 0: // Estado inicial
+                    if (Character.isLetter(c)) {
+                        estado = 1;
                         lexema += c;
                     }
-                    else if(Character.isDigit(c)){
-                        estado = 15;
-                        lexema += c;
 
-                        /*while(Character.isDigit(c)){
-                            lexema += c;
-                            i++;
-                            c = source.charAt(i);
-                        }
-                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
-                        lexema = "";
-                        estado = 0;
-                        tokens.add(t);
-                        */
-
-                    }
-                    break;
-
-                case 13:
-                    if(Character.isLetterOrDigit(c)){
-                        estado = 13;
+                    else if (Character.isDigit(c)) {
+                        estado = 2;
                         lexema += c;
                     }
-                    else{
-                        TipoToken tt = palabrasReservadas.get(lexema);
 
-                        if(tt == null){
-                            Token t = new Token(TipoToken.IDENTIFIER, lexema);
-                            tokens.add(t);
-                        }
-                        else{
-                            Token t = new Token(tt, lexema);
-                            tokens.add(t);
-                        }
-
-                        estado = 0;
-                        lexema = "";
-                        i--;
-
+                    else if (c == '"') {
+                        estado = 8;
+                        lexema += c;
                     }
-                    break;
 
-                case 15:
-                    if(Character.isDigit(c)){
+                    else if (c == '/') {
+                        estado = 10;
+                        lexema += c;
+                    }
+
+                    else if (se.contains(c + "")) {
+                        estado = 14;
+                        lexema += c;
+                    }
+
+                    else if (c == '<') {
                         estado = 15;
                         lexema += c;
                     }
-                    else if(c == '.'){
 
+                    else if (c == '>') {
+                        estado = 16;
+                        lexema += c;
                     }
-                    else if(c == 'E'){
 
+                    else if (c == '=') {
+                        estado = 17;
+                        lexema += c;
                     }
-                    else{
-                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
-                        tokens.add(t);
 
-                        estado = 0;
-                        lexema = "";
-                        i--;
+                    else if (c == '!') {
+                        estado = 18;
+                        lexema += c;
                     }
+
                     break;
             }
 
-
         }
-
 
         return tokens;
     }
